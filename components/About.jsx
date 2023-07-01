bdd7d726d347785d26b6e4b2c96e69d4691266e4
Kemal98 <93825921+Kemal98@users.noreply.github.com>
2023-07-01 11:31:40 +0200

    Update About.jsx

--- components/About.jsx
+++ components/About.jsx
@@ -13,30 +13,19 @@ const About = () => {
       <div className="flex flex-col lgl:flex-row gap-16">
         <div className="w-full lgl:w-2/3 text-base text-textDark font-medium flex flex-col gap-4">
           <p>
-            "Hello! I'm Kemal Merdan, a passionate and experienced software
-            developer with a focus on creating innovative web applications.
-            Throughout my career, I've worked independently on a diverse range
-            of projects, including applications such as Dashboard, App Memories,
-            Movie App, Quizz App, E- commerce web shop, New application,
-            CRM-clone, weather forecast application, social network application
-            and others. I have extensive experience in software development,
-            including a three-month internship at an Austrian company, where I
-            gained valuable insight into industry best practices. I also
-            successfully completed two freelancing projects, one of which
-            involved the development of a comprehensive control panel and the
-            other focused on the redesign of an existing application. During my
-            freelance journey, I had the privilege of working with Unimetal to
-            create a visually stunning website that showcased their brand and
-            offerings. My expertise includes working with REST API, as well as
-            knowledge of Node.js, Express and Mongoose, which allows me to
-            develop scalable and performant applications. As a dedicated
-            developer, I stay up to date with the latest trends and technologies
-            in the industry. I thrive on challenging projects that allow me to
-            fuel my creativity and deliver exceptional software solutions. If
-            you are looking for a professional and reliable developer for your
-            next project, I would be happy to connect with you. Feel free to
-            contact me via email. I look forward to the opportunity to work
-            together.
+"Hello! I'm Kemal Merdan, a passionate and experienced software developer with a focus on creating innovative web 
+            applications. Throughout my career, I've worked independently on a diverse range of projects, including
+            applications such as Dashboard, App Memories, Movie App, Quizz App, E- commerce web shop, New application,
+            CRM-clone, weather forecast application, social network application and others. I have extensive experience
+            in software development, including a three-month internship at an Austrian company, where I gained valuable
+            insight into industry best practices. I also successfully completed two freelancing projects, one of which
+            involved the development of a comprehensive control panel and the other focused on the redesign of an existing
+            application. During my freelance journey, I had the privilege of working with Unimetal to create a visually stunning
+            website that showcased their brand and offerings. My expertise includes working with REST API, as well as knowledge of Node.js,
+            Express and Mongoose, which allows me to develop scalable and performant applications.  Feel free to contact me via email. 
+If you need a professional and reliable developer, feel free to contact me via email. Let's work together!"
+
+
           </p>
           <ul className="max-w-[450px] text-sm font-titleFont grid grid-cols-2 gap-2 mt-6">
             <li className="flex items-center gap-2">
